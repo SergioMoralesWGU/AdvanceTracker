f78d48942ba430ddd66cae00e2f19b3baf333694
Sergio Morales <svartingknas@gmail.com>
2020-02-25 09:12:15 -0700

    adds notifications, need to figure out date

--- app/src/main/java/com/svartingknas/wguadvancetracker/MyReceiver.java
+++ app/src/main/java/com/svartingknas/wguadvancetracker/MyReceiver.java
@@ -10,50 +10,74 @@ import android.os.Build;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class MyReceiver extends BroadcastReceiver {
     static int notificationID;
-    String channel_id="test";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context,intent.getStringExtra("key"),Toast.LENGTH_LONG).show();
-        createNotificationChannel(context,channel_id);
-  /*      Notification n=new Notification.Builder(context)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setChannelId(channel_id)
-                .setContentTitle("Test Notification with an id of:"+Integer.toString(notificationID))
-                .setContentText("This is a test").build();*/
-        Notification n= new NotificationCompat.Builder(context, channel_id)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentText(intent.getStringExtra("key"))
-                .setContentTitle("Test of Notification with an id of :"+Integer.toString(notificationID)).build();
-
-        NotificationManager notificationManager=(NotificationManager)context.getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(notificationID++,n);
-
-        //Put a notification her aka Vogella Tutorial
-
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        // throw new UnsupportedOperationException("Not yet implemented");
+        Toast.makeText(context, "notification", Toast.LENGTH_LONG).show();
+        String channel = intent.getStringExtra("channel");
+        String startClass = intent.getStringExtra("startClass");
+        createNotificationChannel(context, channel);
+        Notification n = new NotificationCompat.Builder(context, channel)
+                .setSmallIcon(R.drawable.ic_event_note_black_24dp)
+                .setContentTitle("Test notification")
+                .setContentText(startClass).build();
+        NotificationManager notificationManager= (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(notificationID++, n);
     }
+
     private void createNotificationChannel(Context context, String CHANNEL_ID) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = context.getResources().getString(R.string.channel_name);
-            String description = context.getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "ted", NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("test channel");
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 }
+//    static int notificationID;
+//    String channel_id = "test";
+
+
+//        Toast.makeText(context,intent.getStringExtra("key"),Toast.LENGTH_LONG).show();
+//        createNotificationChannel(context,channel_id);
+//  /*      Notification n=new Notification.Builder(context)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setChannelId(channel_id)
+//                .setContentTitle("Test Notification with an id of:"+Integer.toString(notificationID))
+//                .setContentText("This is a test").build();*/
+//        Notification n= new NotificationCompat.Builder(context, channel_id)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentText(intent.getStringExtra("key"))
+//                .setContentTitle("Test of Notification with an id of :"+Integer.toString(notificationID)).build();
+//
+//        NotificationManager notificationManager=(NotificationManager)context.getSystemService(NOTIFICATION_SERVICE);
+//        notificationManager.notify(notificationID++,n);
+//
+//        //Put a notification her aka Vogella Tutorial
+//
+//        // TODO: This method is called when the BroadcastReceiver is receiving
+//        // an Intent broadcast.
+//        // throw new UnsupportedOperationException("Not yet implemented");
+//    }
+//    private void createNotificationChannel(Context context, String CHANNEL_ID) {
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = context.getResources().getString(R.string.channel_name);
+//            String description = context.getString(R.string.channel_description);
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+//            channel.setDescription(description);
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//    }
+//}
